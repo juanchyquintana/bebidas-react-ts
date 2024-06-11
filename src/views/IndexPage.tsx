b14d01e8e7f761ee9dc4657520c8e91f3aaf26d0
Juan Diego <quintanajuanchy25@gmail.com>
2024-06-11 16:12:00 -0300

    se agregó zustand y se crearon los stores para iniciar el trabajo con la api

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,6 +1,8 @@
-import Header from "../components/Header";
+import { useAppStore } from "../stores/useAppStore"
 
 export default function IndexPage() {
+  const categories = useAppStore((state) => state.categories)
+  
   return (
     <>
         <h1>Inicio</h1>
